56d64fd03897a2402262f16a42e4d8d969f2e7ba
kinggigo kinggigo <donghaa90@naver.com>
2018-07-05 18:02:45 +0900

    20180705

--- AutoTradDH/Form1.Designer.cs
+++ AutoTradDH/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.logoutBt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(12, 12);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(100, 50);
+            this.axKHOpenAPI1.TabIndex = 0;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(37, 68);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "로그인";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(128, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(230, 280);
+            this.listBox1.TabIndex = 2;
+            // 
+            // logoutBt
+            // 
+            this.logoutBt.Location = new System.Drawing.Point(37, 98);
+            this.logoutBt.Name = "logoutBt";
+            this.logoutBt.Size = new System.Drawing.Size(75, 23);
+            this.logoutBt.TabIndex = 3;
+            this.logoutBt.Text = "로그아웃";
+            this.logoutBt.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(422, 373);
+            this.Controls.Add(this.logoutBt);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button logoutBt;
     }
 }
 
--- AutoTradDH/Form1.cs
+++ AutoTradDH/Form1.cs
@@ -15,6 +15,55 @@ namespace AutoTradDH
         public Form1()
         {
             InitializeComponent();
+            this.loginButton.Click += this.ButtonCliked;
+            this.logoutBt.Click += this.ButtonCliked; 
+            this.axKHOpenAPI1.OnEventConnect += this.axKHOpenAPI1_OnEventConnect;
         }
+        public void ButtonCliked(object sender, EventArgs e)
+        {
+            if (sender.Equals(this.loginButton))
+            {
+                if (axKHOpenAPI1.CommConnect() == 0)
+                {
+                    this.listBox1.Items.Add("로그인 시작");
+                }
+                else
+                    this.listBox1.Items.Add("로그인 실패");
+            }else if (sender.Equals(this.logoutBt))
+            {
+                axKHOpenAPI1.CommTerminate();
+                //로그아웃 하는 함수  CommTerminate이제 안먹힘
+                this.listBox1.Items.Add(axKHOpenAPI1.GetConnectState());
+                if (axKHOpenAPI1.GetConnectState().Equals(0))
+                { this.listBox1.Items.Add("로그아웃"); }
+            }
+            
+        }
+
+       
+
+        public void axKHOpenAPI1_OnEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            if (e.nErrCode == 0)
+            {
+                this.listBox1.Items.Add("로그인성공");
+            }
+            else
+                this.listBox1.Items.Add("로그인 실패");
+
+
+            getlogInfo();
+        }
+
+        public void getlogInfo()
+        {
+            this.axKHOpenAPI1.GetLoginInfo("USER_ID");
+            this.axKHOpenAPI1.GetLoginInfo("USER_NAME");
+        }
+
+
+
+
+
     }
 }
