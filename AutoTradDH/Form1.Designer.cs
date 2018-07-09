d0a7999101c20a5106765f6b3a06f8ac5cfe3943
kinggigo kinggigo <donghaa90@naver.com>
2018-07-09 16:52:40 +0900

    계좌정보가져오기 완료

--- AutoTradDH/Form1.Designer.cs
+++ AutoTradDH/Form1.Designer.cs
@@ -40,7 +40,13 @@
             this.itemCodeText = new System.Windows.Forms.TextBox();
             this.ItemSerchbt = new System.Windows.Forms.Button();
             this.listBox2 = new System.Windows.Forms.ListBox();
+            this.list_realtime = new System.Windows.Forms.ListBox();
+            this.dailybt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -66,9 +72,9 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(128, 68);
+            this.listBox1.Location = new System.Drawing.Point(679, 59);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(230, 280);
+            this.listBox1.Size = new System.Drawing.Size(230, 148);
             this.listBox1.TabIndex = 2;
             // 
             // logoutBt
@@ -83,7 +89,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(1, 127);
+            this.comboBox1.Location = new System.Drawing.Point(407, 33);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 20);
             this.comboBox1.TabIndex = 4;
@@ -91,7 +97,7 @@
             // NAMElb
             // 
             this.NAMElb.AutoSize = true;
-            this.NAMElb.Location = new System.Drawing.Point(140, 49);
+            this.NAMElb.Location = new System.Drawing.Point(691, 40);
             this.NAMElb.Name = "NAMElb";
             this.NAMElb.Size = new System.Drawing.Size(29, 12);
             this.NAMElb.TabIndex = 5;
@@ -100,7 +106,7 @@
             // IDlb
             // 
             this.IDlb.AutoSize = true;
-            this.IDlb.Location = new System.Drawing.Point(249, 49);
+            this.IDlb.Location = new System.Drawing.Point(800, 40);
             this.IDlb.Name = "IDlb";
             this.IDlb.Size = new System.Drawing.Size(41, 12);
             this.IDlb.TabIndex = 6;
@@ -109,7 +115,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(366, 49);
+            this.label1.Location = new System.Drawing.Point(917, 40);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 7;
@@ -117,14 +123,14 @@
             // 
             // itemCodeText
             // 
-            this.itemCodeText.Location = new System.Drawing.Point(421, 46);
+            this.itemCodeText.Location = new System.Drawing.Point(972, 37);
             this.itemCodeText.Name = "itemCodeText";
             this.itemCodeText.Size = new System.Drawing.Size(100, 21);
             this.itemCodeText.TabIndex = 8;
             // 
             // ItemSerchbt
             // 
-            this.ItemSerchbt.Location = new System.Drawing.Point(527, 44);
+            this.ItemSerchbt.Location = new System.Drawing.Point(1078, 35);
             this.ItemSerchbt.Name = "ItemSerchbt";
             this.ItemSerchbt.Size = new System.Drawing.Size(75, 23);
             this.ItemSerchbt.TabIndex = 9;
@@ -135,24 +141,85 @@
             // 
             this.listBox2.FormattingEnabled = true;
             this.listBox2.ItemHeight = 12;
-            this.listBox2.Location = new System.Drawing.Point(368, 68);
+            this.listBox2.Location = new System.Drawing.Point(919, 59);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(234, 400);
             this.listBox2.TabIndex = 10;
             // 
+            // list_realtime
+            // 
+            this.list_realtime.FormattingEnabled = true;
+            this.list_realtime.ItemHeight = 12;
+            this.list_realtime.Location = new System.Drawing.Point(679, 239);
+            this.list_realtime.Name = "list_realtime";
+            this.list_realtime.Size = new System.Drawing.Size(229, 280);
+            this.list_realtime.TabIndex = 11;
+            // 
+            // dailybt
+            // 
+            this.dailybt.Location = new System.Drawing.Point(26, 288);
+            this.dailybt.Name = "dailybt";
+            this.dailybt.Size = new System.Drawing.Size(85, 28);
+            this.dailybt.TabIndex = 12;
+            this.dailybt.Text = "일봉조회";
+            this.dailybt.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-1, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 12);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "기준일자(ex 20180709)";
+            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(21, 377);
+            this.textBox1.Location = new System.Drawing.Point(26, 263);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(174, 21);
-            this.textBox1.TabIndex = 11;
+            this.textBox1.Size = new System.Drawing.Size(85, 21);
+            this.textBox1.TabIndex = 14;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(26, 322);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.Text = "연속조회";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 12;
+            this.listBox3.Location = new System.Drawing.Point(407, 59);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(266, 448);
+            this.listBox3.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(405, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "계좌번호";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(740, 575);
+            this.ClientSize = new System.Drawing.Size(1214, 582);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dailybt);
+            this.Controls.Add(this.list_realtime);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.ItemSerchbt);
             this.Controls.Add(this.itemCodeText);
@@ -185,7 +252,13 @@
         private System.Windows.Forms.TextBox itemCodeText;
         private System.Windows.Forms.Button ItemSerchbt;
         private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox list_realtime;
+        private System.Windows.Forms.Button dailybt;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- AutoTradDH/Form1.cs
+++ AutoTradDH/Form1.cs
@@ -18,9 +18,33 @@ namespace AutoTradDH
             this.loginButton.Click += this.ButtonCliked;
             this.logoutBt.Click += this.ButtonCliked;
             this.ItemSerchbt.Click += this.ButtonCliked;
+            //일봉차트 클릭
+            this.dailybt.Click += this.ButtonCliked;
             this.axKHOpenAPI1.OnEventConnect += this.axKHOpenAPI1_OnEventConnect;
             this.axKHOpenAPI1.OnReceiveTrData += this.axKHOpenAPI1_OnReceiverTrData;
+            this.axKHOpenAPI1.OnReceiveRealData += this.axKHOPenAPI_OnReceiveRealData;
+
+            //계좌정보조회
+            this.comboBox1.SelectedIndexChanged += this.comboBox_Selecteindexchanged;
         }
+
+        //계좌정보
+        private void comboBox_Selecteindexchanged(object sender, System.EventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            //계좌정보 입력
+            String selecteAccount = (string)comboBox.SelectedItem;
+            axKHOpenAPI1.SetInputValue("계좌번호", selecteAccount.Trim());
+            axKHOpenAPI1.SetInputValue("비밀번호", "");
+            axKHOpenAPI1.SetInputValue("상장폐지조회구분", "0");
+            axKHOpenAPI1.SetInputValue("비밀번호입력매체구분", "00");
+
+            this.listBox3.Items.Clear();
+
+            int nRet = axKHOpenAPI1.CommRqData("계좌평가현황요청", "opw00004", 0, "6001");
+
+        }
+
         public void ButtonCliked(object sender, EventArgs e)
         {
             if (sender.Equals(this.loginButton))
@@ -52,7 +76,23 @@ namespace AutoTradDH
                 else
                     this.listBox1.Items.Add("주식정보요청 실패");
                     
-            }
+            }else if (sender.Equals(this.dailybt))
+            {
+                axKHOpenAPI1.SetInputValue("종목코드", this.itemCodeText.Text.Trim());
+                axKHOpenAPI1.SetInputValue("기준일자", this.textBox1.Text.Trim());
+                axKHOpenAPI1.SetInputValue("수정주가구분", "1");
+
+                int nRet;
+                if (!this.checkBox1.Checked)
+                    nRet = axKHOpenAPI1.CommRqData("주식일봉차트조회", "opt10081", 0, "1002");
+                else
+                    nRet = axKHOpenAPI1.CommRqData("주식일봉차트조회", "opt10081", 2, "1002");
+
+                if (nRet == 0)
+                    this.listBox1.Items.Add("주식일봉 요청 성공");
+                else
+                    this.listBox1.Items.Add("주식일봉 요청 실패");
+                        }
         }
 
        
@@ -99,9 +139,84 @@ namespace AutoTradDH
                     this.listBox2.Items.Add("현재가" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "현재가"));
                     this.listBox2.Items.Add("등락율" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "등락율"));
                 
+            }else if(e.sRQName == "주식일봉차트조회")
+            {
+                int nCnt = axKHOpenAPI1.GetRepeatCnt(e.sTrCode, e.sRQName);
+                for(int i =0; i < nCnt; i++)
+                {
+                    this.listBox2.Items.Add("일자" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "일자").Trim());
+                    this.listBox2.Items.Add("현재가" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "일자").Trim());
+                    this.listBox2.Items.Add("거래량" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "일자").Trim());
+                    this.listBox2.Items.Add("시가" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "일자").Trim());
+                    this.listBox2.Items.Add("거기" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "일자").Trim());
+                    this.listBox2.Items.Add("저가" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "일자").Trim());
+                    this.listBox2.Items.Add("=========================구분선=================================");
+                    this.listBox2.SelectedIndex = this.listBox2.Items.Count - 1;
+
+                }
+            }else if(e.sRQName == "계좌평가현황요청")
+            {
+                this.listBox3.Items.Add("예수금 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "예수금").Trim()));
+                this.listBox3.Items.Add("d+2추정예수금 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "D+2추정예수금").Trim()));
+                this.listBox3.Items.Add("유가잔고평가액 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "유가잔고평가액").Trim()));
+                this.listBox3.Items.Add("예탁자산평가액 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "예탁자산평가액").Trim()));
+                this.listBox3.Items.Add("총매입금액 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "총매입금액").Trim()));
+                this.listBox3.Items.Add("추정예탁자산 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "추정예탁자산").Trim()));
+                this.listBox3.Items.Add("당일투자원금 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "당일투자원금").Trim()));
+                this.listBox3.Items.Add("당월투자원금 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "당월투자원금").Trim()));
+                this.listBox3.Items.Add("누적투자원금 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "누적투자원금").Trim()));
+                this.listBox3.Items.Add("당일투자손익 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "당일투자손익").Trim()));
+                this.listBox3.Items.Add("당월투자손익 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "당월투자손익").Trim()));
+                this.listBox3.Items.Add("누적투자손익 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "누적투자손익").Trim()));
+                this.listBox3.Items.Add("당일손익율 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "당일손익율").Trim()));
+                this.listBox3.Items.Add("당월손익율 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "당월손익율").Trim()));
+                this.listBox3.Items.Add("누적손익율 =" + Int32.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "누적손익율").Trim()));
+
+                int nCnt = axKHOpenAPI1.GetRepeatCnt(e.sTrCode, e.sRQName);
+                for(int i=0; i < nCnt; i++)
+                {
+                    this.listBox3.Items.Add("종목명 = " + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "종목명").Trim());
+                    this.listBox3.Items.Add("종목코드 = " + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "종목코드").Trim());
+                    this.listBox3.Items.Add("보유수량 = " + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "보유수량").Trim());
+                    this.listBox3.Items.Add("현재가 = " + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "현재가").Trim());
+                    this.listBox3.Items.Add("평가금액 = " + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "평가금액").Trim());
+                    this.listBox3.Items.Add("손익금액 = " + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "손익금액").Trim());
+                    this.listBox3.Items.Add("손익율 = " + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "손익율").Trim());
+                    this.listBox3.Items.Add("매입금액 = " + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "매입금액").Trim());
+                    this.listBox3.Items.Add("결제잔고 = " + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "결제잔고").Trim());
+                    this.listBox3.Items.Add("금일매수수량 = " + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "금일매수수량").Trim());
+                    this.listBox3.Items.Add("금일매도수량 = " + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "금일매도수량").Trim());
+                    
+                }
+
+             
             }
         }
 
+       private void axKHOPenAPI_OnReceiveRealData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveRealDataEvent e)
+        {
+            this.list_realtime.Items.Add(e.sRealData);
+            this.list_realtime.Items.Add(e.sRealKey);
+            this.list_realtime.Items.Add(e.sRealType);
+            this.list_realtime.Items.Add(axKHOpenAPI1.GetCommRealData(e.sRealData, 10).Trim());
+            if(e.sRealType == "주식시세")
+            {
+                this.list_realtime.Items.Add("현재가 = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 10).Trim());
+                this.list_realtime.Items.Add("전일대비 = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 11).Trim());
+                this.list_realtime.Items.Add("등락율 = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 12).Trim());
+                this.list_realtime.Items.Add("(최우선)매도호가 = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 27).Trim());
+                this.list_realtime.Items.Add("(최우선)매수호가 = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 28).Trim());
+                this.list_realtime.Items.Add("누적 거래량 = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 13).Trim());
+                this.list_realtime.Items.Add("누적 거래대금 = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 14).Trim());
+                this.list_realtime.Items.Add("시가 = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 16).Trim());
+                this.list_realtime.Items.Add("고가 = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 17).Trim());
+                this.list_realtime.Items.Add("저가 = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 18).Trim());
+                this.list_realtime.Items.Add("전일대비기호 = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 25).Trim());
+                this.list_realtime.Items.Add("전일거래량대비(계약,주) = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 26).Trim());
+                this.list_realtime.Items.Add("거래다금 증감 = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 27).Trim());
+                this.list_realtime.Items.Add("전일 거래량댜비(비율) = " + axKHOpenAPI1.GetCommRealData(e.sRealData, 30).Trim());
 
+            }
+        }
     }
 }
